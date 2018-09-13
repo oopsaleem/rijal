797c7553126389dea553fc1d6061d1f1af0d01ef
kgwb <kgwb@kgwbs-MacBook-Pro.local>
2018-09-13 22:09:01 +0300

    running

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rijal/main.dart';
 import 'package:rijal/src/scope_model_wrapper.dart';
 
 void main() {
