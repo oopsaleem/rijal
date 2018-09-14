876f1035e5067ecdfb3cf7c99cfab581a75e7425
oopsaleem <robidy.ab@gmail.com>
2018-09-14 14:46:47 +0300

    hi

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,8 +6,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:rijal/src/scope_model_wrapper.dart';
+import 'package:rijal/src/app_model.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
